8b852c44bf70141691f14edb3cae8f162faf9414
trauty <trautwein.maximilian04@gmail.com>
2024-10-04 02:41:43 +0200

    added scene bundler and sdk code

---
+++ ImmerzaSDK/Types/ValueField.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace ImmerzaSDK.Types
+{
+    public enum ValueType
+    {
+        SingleValue,
+        SingleReference,
+        ArrayValue,
+        ArrayReference,
+        AssetReference
+    }
+
+    public class ValueField
+    {
+        public string value;
+        public Type type;
+        [JsonProperty("serialization_type")]
+        public ValueType serializationType;
+    }
+}
