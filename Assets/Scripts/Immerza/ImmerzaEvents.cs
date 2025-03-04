ebbfec2fdc70ffc73029a7bcaf56973283caedf5
trauty <trautwein.maximilian04@gmail.com>
2025-03-04 17:07:01 +0100

    added event raisers

--- Assets/Scripts/Immerza/ImmerzaEvents.cs
+++ Assets/Scripts/Immerza/ImmerzaEvents.cs
@@ -8,5 +8,10 @@ namespace ImmerzaSDK
         public static event Action OnSceneStart;
 
         public static event Action OnSceneEnd;
+
+        public static void EndScene()
+        {
+            OnSceneEnd?.Invoke();
+        }
     }
 }
