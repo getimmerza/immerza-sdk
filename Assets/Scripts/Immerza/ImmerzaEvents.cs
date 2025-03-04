0aee444f95fdbccf61ccdad8a858f72f43ae4d18
trauty <trautwein.maximilian04@gmail.com>
2025-03-04 14:13:41 +0100

    0.4.1 - added ImmerzaEvents class

---
+++ Assets/Scripts/Immerza/ImmerzaEvents.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace ImmerzaSDK
+{
+    public static class ImmerzaEvents
+    {
+        public static event Action<bool> OnPauseRequested; // true for pause, false for unpause
+        public static event Action OnSceneStart;
+    }
+}
